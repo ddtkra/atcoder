e56fc46df9b9b7311f874c8911e07e98f1202354
Akira Takano <deritefully@gmail.com>
2019-09-26 00:16:45 +0900

    ABC124-Dを解きました。解説AC
    累積和で解いたが、問題から適切なアルゴリズムにまだまだ落とせない・・・。
    
    https://atcoder.jp/contests/abc124/submissions/7695641

--- abc124/D/main.py
+++ abc124/D/main.py
@@ -3,24 +3,38 @@ import sys
 
 
 def solve(N: int, K: int, S: str):
-    sel = []
-    ts = 0
-    for i in range(len(S)):
-        if(S[i] == '0'):
-            ts += 1
-        elif(S[i] == '1'):
-            if(ts > 0):
-                sel.append(ts)
-            ts = 0
+    seq = []
+    if(S[0] == '0'): seq.append(0)
+    f = S[0]
+    c = 1
+    for i in range(1, N):
+        if(S[i] == f):
+            c += 1
+        else:
+            seq.append(c)
+            f = S[i]
+            c = 1
+    seq.append(c)
+    
+    rseq = [0] * (len(seq)+1)
+    for i in range(1,len(seq)+1):
+        rseq[i] = rseq[i-1] + seq[i-1]
 
-    sel.sort(reverse=True)
-    ans = S.count('1')
-    print(ans)
-    print(sel)
-    for i in range(min(len(sel),K)):
-        ans += sel[i]
+    ans = 0
+    # cumulative sum
+    for i in range(len(rseq)):
+        # print(ans, rseq[min(len(rseq)-1,i*2+2*K+2)], rseq[min(len(rseq)-1, i*2)])
+        ans = max(ans, rseq[min(len(rseq)-1,i*2+2*K+1)] - rseq[min(len(rseq)-1, i*2)])
+
+
+    # naive
+    # for i in range(len(seq)):
+    #     for j in range(K):
+    #         # print(seq[i*2:i*2+2*K+1])
+    #         ans = max(ans, sum(seq[i*2:i*2+2*K+1]))
 
     print(ans)
+
     return
 
 
