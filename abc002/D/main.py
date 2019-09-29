a15096065b7f782512f829509cd512fef491a208
tk <deritefully@gmail.com>
2019-09-29 22:54:49 +0900

    ABC002-Dを解きました。解説AC bit全探索 N <= 10なので、全員の組み合わせを愚直に調べても間に合う。 フラグの更新に応じてfor文から抜け出させようとしたが、それのせいで大量のWAを発生させた。 for-break-elseの扱い難しい。
    https://atcoder.jp/contests/abc002/submissions/7789969

--- abc002/D/main.py
+++ abc002/D/main.py
@@ -1,8 +1,35 @@
 #!/usr/bin/env python3
 import sys
-
+import numpy
 
 def solve(N: int, M: int, x: "List[int]", y: "List[int]"):
+    
+    ans = 1
+    
+    xy = [[0]*(N+1) for i in range(N+1)]
+    for i in range(M):
+        xy[x[i]][y[i]] = 1
+        xy[y[i]][x[i]] = 1
+
+    for i in range(1<<N):
+        l = []
+        for j in range(N):
+            if (i>>j)&1 :
+                l.append(j+1)
+        
+        nl = len(l)
+        f = True
+        for s in range(nl):
+            for t in range(nl):
+                if(s == t): continue
+                if(xy[l[s]][l[t]] == 0):
+                    f = False
+        
+        if(f):
+            ans = max(ans,len(l))
+
+    print(ans)
+
     return
 
 
