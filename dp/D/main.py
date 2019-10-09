69e3c6523e326eb6b43d226bdd53bd552e7dab22
tk <deritefully@gmail.com>
2019-10-09 17:57:41 +0900

    Educational DP-D を解きました。TLE AC
    螺旋本に乗っていたナップサック問題と完全に同じ。
    これすら理解が曖昧だったのかと絶望したが、なんとか飲み込めた。
    Python3では通らなかったのでPyPy3で通した。
    
    https://atcoder.jp/contests/dp/submissions/7912899

--- dp/D/main.py
+++ dp/D/main.py
@@ -3,6 +3,17 @@ import sys
 
 
 def solve(N: int, W: int, w: "List[int]", v: "List[int]"):
+    dp = [[0] * (W+1) for i in range(N+1)]
+
+    for i in range(1,N+1):
+        for j in range(1,W+1):
+            if j-w[i-1] >= 0:
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j-w[i-1]]+v[i-1])
+            else:
+                dp[i][j] = dp[i-1][j]
+
+    print(dp[N][W])
+
     return
 
 
