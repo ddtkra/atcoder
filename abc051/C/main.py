005366996ce94f778504e792fce01a142e077338
tk <deritefully@gmail.com>
2019-11-16 00:03:00 +0900

    ABC051C できた

--- abc051/C/main.py
+++ abc051/C/main.py
@@ -3,6 +3,14 @@ import sys
 
 
 def solve(sx: int, sy: int, tx: int, ty: int):
+    x = tx-sx
+    y = ty-sy
+
+    print('U'*y+'R'*x
+        +'D'*y+'L'*x
+        +'L'+'U'*(y+1)+'R'*(x+1)+'D'
+        +'R'+'D'*(y+1)+'L'*(x+1)+'U')
+
     return
 
 
