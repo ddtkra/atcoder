4a6987056c9b1dc5c9c6b36582700f6ff013fb88
tk <deritefully@gmail.com>
2019-11-27 23:13:54 +0900

    DISCO2020-B, 塁積和 要復習

---
+++ ddcc2020-qual/F/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import sys
+sys.setrecursionlimit(10000000)
+INF = 1<<32
+
+MOD = 1000000007  # type: int
+
+def solve(H: int, W: int, T: int):
+    return
+
+
+
+def main():
+    def iterate_tokens():
+        for line in sys.stdin:
+            for word in line.split():
+                yield word
+    tokens = iterate_tokens()
+    H = int(next(tokens))  # type: int
+    W = int(next(tokens))  # type: int
+    T = int(next(tokens))  # type: int
+    solve(H, W, T)
+
+if __name__ == '__main__':
+    main()
