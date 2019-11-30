b784f8346e8a46323e52b44fa2889f845e95fe59
tk <deritefully@gmail.com>
2019-11-30 23:34:17 +0900

    kyomu

--- ddcc2020-qual/A/main.py
+++ ddcc2020-qual/A/main.py
@@ -5,6 +5,26 @@ INF = 1<<32
 
 
 def solve(X: int, Y: int):
+    ans = 0
+    if X == 3:
+        ans += 1
+    elif X == 2:
+        ans += 2
+    elif X == 1:
+        ans += 3
+    
+    if Y == 3:
+        ans += 1
+    elif Y == 2:
+        ans += 2
+    elif Y == 1:
+        ans += 3
+    
+    if X == 1 and Y == 1:
+        ans += 4
+    
+    print(ans*10**5)
+
     return
 
 
