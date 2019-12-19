b9590c2e5659c6b40867f5d97883692234ea4503
tk <deritefully@gmail.com>
2019-12-20 00:25:39 +0900

    agc016-A, 要復習 シミュレーション

---
+++ agc016/B/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import sys
+sys.setrecursionlimit(10000000)
+INF = 1<<32
+
+YES = "Yes"  # type: str
+NO = "No"  # type: str
+
+def solve(N: int, a: "List[int]"):
+    return
+
+
+
+def main():
+    def iterate_tokens():
+        for line in sys.stdin:
+            for word in line.split():
+                yield word
+    tokens = iterate_tokens()
+    N = int(next(tokens))  # type: int
+    a = [int(next(tokens)) for _ in range(N)]  # type: "List[int]"
+    solve(N, a)
+
+if __name__ == '__main__':
+    main()
---
+++ agc016/C/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import sys
+sys.setrecursionlimit(10000000)
+INF = 1<<32
+
+YES = "Yes"  # type: str
+NO = "No"  # type: str
+
+def solve(H: int, W: int, h: int, w: int):
+    return
+
+
+
+def main():
+    def iterate_tokens():
+        for line in sys.stdin:
+            for word in line.split():
+                yield word
+    tokens = iterate_tokens()
+    H = int(next(tokens))  # type: int
+    W = int(next(tokens))  # type: int
+    h = int(next(tokens))  # type: int
+    w = int(next(tokens))  # type: int
+    solve(H, W, h, w)
+
+if __name__ == '__main__':
+    main()
