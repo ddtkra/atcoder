e1e4ed4e5ffd38c2e5a87970086fadb1277dffe7
tk <deritefully@gmail.com>
2019-12-24 22:33:46 +0900

    abc148-A-E

---
+++ abc148/A/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import sys
+sys.setrecursionlimit(10000000)
+INF = 1<<32
+
+
+def solve(A: int, B: int):
+    x = {1,2,3}-{A,B}
+    print(x.pop())
+    return
+
+
+
+def main():
+    def iterate_tokens():
+        for line in sys.stdin:
+            for word in line.split():
+                yield word
+    tokens = iterate_tokens()
+    A = int(next(tokens))  # type: int
+    B = int(next(tokens))  # type: int
+    solve(A, B)
+
+if __name__ == '__main__':
+    main()
---
+++ abc148/F/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import sys
+sys.setrecursionlimit(10000000)
+INF = 1<<32
+
+
+def solve(N: int, u: int, v: int, A: "List[int]", B: "List[int]"):
+    return
+
+
+
+def main():
+    def iterate_tokens():
+        for line in sys.stdin:
+            for word in line.split():
+                yield word
+    tokens = iterate_tokens()
+    N = int(next(tokens))  # type: int
+    u = int(next(tokens))  # type: int
+    v = int(next(tokens))  # type: int
+    A = [int()] * (N - 1)  # type: "List[int]"
+    B = [int()] * (N - 1)  # type: "List[int]"
+    for i in range(N - 1):
+        A[i] = int(next(tokens))
+        B[i] = int(next(tokens))
+    solve(N, u, v, A, B)
+
+if __name__ == '__main__':
+    main()
