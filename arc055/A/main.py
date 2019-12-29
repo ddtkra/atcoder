e155869af2a60a4258cff4a300744fe049e41d24
tk <deritefully@gmail.com>
2019-12-29 19:59:31 +0900

    kyomu

---
+++ arc055/A/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import sys
+sys.setrecursionlimit(10000000)
+INF = 1<<32
+
+
+def solve(N: int):
+    print('1'+'0'*(N-1)+'7')
+    return
+
+
+
+def main():
+    def iterate_tokens():
+        for line in sys.stdin:
+            for word in line.split():
+                yield word
+    tokens = iterate_tokens()
+    N = int(next(tokens))  # type: int
+    solve(N)
+
+if __name__ == '__main__':
+    main()
