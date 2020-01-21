a8b89fce6fd82bba9541cbea16ab1addb99d0483
tk <deritefully@gmail.com>
2020-01-21 21:56:37 +0900

    dwacon

---
+++ dwacon6th-prelims/C/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import sys
+sys.setrecursionlimit(10000000)
+INF = 1<<32
+
+MOD = 1000000007  # type: int
+
+def solve(N: int, K: int, a: "List[int]"):
+    return
+
+
+
+def main():
+    def iterate_tokens():
+        for line in sys.stdin:
+            for word in line.split():
+                yield word
+    tokens = iterate_tokens()
+    N = int(next(tokens))  # type: int
+    K = int(next(tokens))  # type: int
+    a = [int(next(tokens)) for _ in range(K)]  # type: "List[int]"
+    solve(N, K, a)
+
+if __name__ == '__main__':
+    main()
