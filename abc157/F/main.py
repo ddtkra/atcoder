ef12e28ef4c6bedfd1b7b5dde3c3adf954f4329e
tk <deritefully@gmail.com>
2020-03-07 20:50:30 +0900

    abc157

---
+++ abc157/F/main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import sys
+sys.setrecursionlimit(10000000)
+INF = 1<<32
+
+
+def solve(N: int, K: int, x: "List[int]", y: "List[int]", c: "List[int]"):
+    return
+
+
+
+def main():
+    def iterate_tokens():
+        for line in sys.stdin:
+            for word in line.split():
+                yield word
+    tokens = iterate_tokens()
+    N = int(next(tokens))  # type: int
+    K = int(next(tokens))  # type: int
+    x = [int()] * (N)  # type: "List[int]"
+    y = [int()] * (N)  # type: "List[int]"
+    c = [int()] * (N)  # type: "List[int]"
+    for i in range(N):
+        x[i] = int(next(tokens))
+        y[i] = int(next(tokens))
+        c[i] = int(next(tokens))
+    solve(N, K, x, y, c)
+
+if __name__ == '__main__':
+    main()
